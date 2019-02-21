c7d5bfed4fa904276d676fdd13853cdf4bb6695f
Fabian Meyer (Laser-Controller) <fabian.meyer@ise.fhg.de>
2019-02-21 16:32:39 +0100

    fixed make_gui(fp) for fp beiing filepath

--- defaults.py
+++ defaults.py
@@ -27,7 +27,7 @@ def make_gui(spim_or_filepath):
     try:
         assert os.path.exists(spim_or_filepath)
         spim = Spim.from_file(image_filepath, cached=True)
-    except:
+    except AssertionError:
         spim = spim_or_filepath
 
     pipe = default_pipeline()
