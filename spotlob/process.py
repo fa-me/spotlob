433fc77f507e391e24adbc44f6c9208be06e1ce1
Fabian Meyer <fabian.meyer@ise.fraunhofer.de>
2019-03-28 12:15:47 +0100

    process doc

--- spotlob/process.py
+++ spotlob/process.py
@@ -1,3 +1,9 @@
+"""
+This module contains abstract super classes for concrete ProcessSteps.
+Processes are applied to a Spim to bring it to the next stage.
+"""
+
+
 import numpy as np
 
 from .spim import SpimStage
@@ -5,7 +11,11 @@ from .parameters import SpotlobParameterSet
 
 
 class SpotlobProcessStep(object):
-    """A super class for process steps"""
+    """An abstract super class for process steps. A process step can be
+    applied to bring a spim from one stage to another. It is supposed to save
+    as internal state, if it has already been applied or needs to be applied
+    again, because the parameters have changed. This is stored in the
+    `outdated` parameter"""
 
     # stage that a spim should have before this Process can be applied
     input_stage = None
@@ -15,6 +25,24 @@ class SpotlobProcessStep(object):
     outdated = True
 
     def __init__(self, function, parameters, add_to_register=True):
+        """
+        A process step can be applied to bring a spim from one stage
+        to another
+
+        Parameters
+        ----------
+        function : function(spim, args) -> spim
+            a function to be applied on spim with optional additional
+            parameters
+        parameters : list of SpotlobParameter
+            the parameters that will be used for the function as soon as
+            it is applied
+        add_to_register : bool, optional
+            if this is True, the process will be registered centrally now, i.e.
+            upon creation of the SpotlobProcessStep object. This allows the
+            user to know which processes are available (the default is True)
+
+        """
         self.function = function
         self.parameters = SpotlobParameterSet(parameters)
 
@@ -31,6 +59,12 @@ class SpotlobProcessStep(object):
         user on how the funciton will work. No storage or sideeffects
         should take place.
         In contrast to the apply function it must always return an image
+
+        Parameters
+        ----------
+        spim : Spim
+            Spim to draw the preview on. It must contain an image
+
         """
         raise NotImplementedError("abstract: to be implemented by subclass")
 
