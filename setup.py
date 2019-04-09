8f6506937bfa99a7ee684c8ba415a11c6566d559
Fabian Meyer <fabian.meyer@ise.fraunhofer.de>
2019-04-09 15:02:09 +0200

    added setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import spotlob
 
 setup(
     name=spotlob.__name__,
     version=spotlob.__version__,
-    packages=["spotlob", "spotlob.tests"],
-    author=spotlob.__version__,
+    author=spotlob.__author__,
     author_email=spotlob.__email__,
     description=spotlob.__summary__,
     license="BSD 3-clause",
