9e13edb7662a043d56bce0c5975400a08af9fb6d
Fabian Meyer <fabian.meyer@ise.fraunhofer.de>
2019-08-13 18:37:09 +0200

    ContourFinder with child/parent selection options

--- spotlob/process_opencv.py
+++ spotlob/process_opencv.py
@@ -166,19 +166,73 @@ class ContourFinder(FeatureFinder):
     """
 
     def __init__(self, mode):
-        pars = SpotlobParameterSet([])
+        mode_par = EnumParameter("mode", mode,
+                                 ["outer",
+                                  "inner",
+                                  "all",
+                                  "holes",
+                                  "non-holes"])
+        pars = SpotlobParameterSet([mode_par])
         super(ContourFinder, self).__init__(self.finder_fn, pars)
 
-    def finder_fn(self, bin_im):
-        cont_ret = cv2.findContours(bin_im,
-                                    cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)
-        # cont_ret is
-        # contours, hierarchy for opencv >4.0
-        # im, contours, hierarchy for opencv <=3.4
+    def finder_fn(self, bin_im, mode):
+        print(mode)
 
-        contours = cont_ret[-2]
-        return contours
+        if mode == "outer":
+            cont_ret = cv2.findContours(bin_im,
+                                        cv2.RETR_EXTERNAL,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+            return cont_ret[-2]
+        else:
+            cont_ret = cv2.findContours(bin_im,
+                                        cv2.RETR_CCOMP,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+            # cont_ret is
+            # contours, hierarchy for opencv >4.0
+            # im, contours, hierarchy for opencv <=3.4
+
+            contours = cont_ret[-2]
+            hierarchy = cont_ret[-1][0]
+
+            if mode == 'all':
+                return contours
+            else:
+                holes = []
+                nonholes = []
+                inner = []
+
+                i = 0
+
+                nonholes_i = []
+
+                # first hierarchy is the nonholes
+                # parse through them
+                while i >= 0:
+                    head = contours[i]
+                    nonholes.append(head)
+                    nonholes_i.append(i)
+
+                    next_i, prev_i, child_i, parent_i = hierarchy[i]
+
+                    if child_i < 0:
+                        inner.append(head)
+
+                    i = next_i  # index of next contour on same hierarchy level
+
+            if mode == "holes":
+                # rest should be holes
+                # ie. all which are not nonholes
+                # list of holes is the contour list withouth the ones
+                # specified by nonholes index list
+                holes = np.delete(contours, nonholes_i, axis=0)
+                return holes
+            elif mode == "non-holes":
+                return nonholes
+            elif mode == "inner":
+                return inner
+            else:
+                raise NotImplementedError(
+                    "Unsupported contour finder mode %s" % mode)
 
 
 class FeatureFormFilter(FeatureFilter):
