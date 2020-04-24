b33adff74721995dc619e66a8f1acaebc8609324
fmeyer <fabian.meyer@ise.fraunhofer.de>
2020-04-25 00:35:21 +0200

    fix missing imports

--- spotlob/__init__.py
+++ spotlob/__init__.py
@@ -1,4 +1,4 @@
 from .version import *
 from .spim import Spim, SpimStage
-from .defaults import default_pipeline, load_image, make_gui, show_gui
+from .defaults import *
 from .review import review_widget
