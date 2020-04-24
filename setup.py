53f08ceb1e540838373c555f3a1691cc87bc85a4
fmeyer <fabian.meyer@ise.fraunhofer.de>
2020-04-25 00:18:45 +0200

    move pkg metadata for risk free import in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
-import spotlob
+import spotlob.version as spotlobversion
 
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(
-    name=spotlob.__name__,
-    version=spotlob.__version__,
-    author=spotlob.__author__,
-    author_email=spotlob.__email__,
-    description=spotlob.__summary__,
+    name=spotlobversion.__name__,
+    version=spotlobversion.__version__,
+    author=spotlobversion.__author__,
+    author_email=spotlobversion.__email__,
+    description=spotlobversion.__summary__,
     license="BSD 3-clause",
-    url=spotlob.__url__,
+    url=spotlobversion.__url__,
     classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Science/Research',
